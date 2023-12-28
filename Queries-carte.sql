61cf6752d2be384cb0291a36f44f53360a703b5c
MrBarso <andbars2911@gmail.com>
2023-12-28 21:14:09 +0100

    altre queries (non lo finirò mi basta questo)

--- Queries-carte.sql
+++ Queries-carte.sql
@@ -4,14 +4,41 @@
 SELECT 
     esercenti.categoria, 
     MONTH(acquisti.data) AS mese_acquisto, 
-    esercente.citta, -- non so come si calcola la regione dato un indirizzo
+    esercenti.citta,
     SUM(acquisti.importo) AS totale_speso
 FROM acquisti 
-JOIN clienti ON acquisti.id_acquisto
+JOIN clienti ON acquisti.id
 GROUP BY 
     acquisti.categoria, 
-    acquisti.data, -- non so come si groupa per trimestre avendo il mese
-    esercente.citta;
+    acquisti.data,
+    esercenti.citta;
 
 
--- numero medio di acquisti nell'articolo di viaggio
+-- numero medio di acquisti degli articoli da viaggio dei clienti con fascia di reddito tra 50.000 e 60.000 per conto e citta
+
+SELECT 
+    YEAR(acquisti.data) AS anno_acquisto, 
+    estratti_conto.id, 
+    esercenti.citta, 
+    AVG(IF(acquisti.categoria = 'viaggi', 1, NULL)) AS acquisti_articoliviaggio_media
+FROM acquisti
+JOIN estratto_conto ON acquisti.id_acquisto
+JOIN clienti ON acquisti.id_acquisto
+WHERE 
+    anno_acquisto = 2023
+    AND clienti.fascia_reddito >= 50000
+    AND clienti.fascia_reddito < 60000
+GROUP BY 
+    estratti_conto.id, 
+    clienti.indirizzo
+
+-- totale speso in viaggi per mese e provincia
+SELECT
+    SUM(IF(acquisti.categoria = 'viaggi', 1, NULL)) AS totale,
+    MONTH(acquisti.data) AS mese_acquisto, 
+    esercenti.citta
+FROM acquisti
+JOIN clienti ON acquisti.id
+GROUP BY 
+    mese_acquisto, 
+    clienti.indirizzo
