69163ea12766ae4a0a6d126f87b436e38d3de73a
MrBarso <129418685+MrBarso@users.noreply.github.com>
2024-01-26 13:46:24 +0100

    Update CreazioneDB-carte.sql

--- CreazioneDB-carte.sql
+++ CreazioneDB-carte.sql
@@ -3,32 +3,32 @@ CREATE DATABASE carte;
 
 --creazione tabella clienti
 CREATE TABLE clienti (
-    'cf' VARCHAR(16) NOT NULL PRIMARY KEY,
-    'nome' VARCHAR(20) NOT NULL,
-    'fascia_reddito' FLOAT NOT NULL,
-    'sesso' ENUM('M', 'F') NOT NULL,
-    'data_nascita' DATE NOT NULL,
-    'indirizzo' VARCHAR(30) NOT NULL,
-    'saldo' FLOAT NOT NULL,
-    'numero_carta'  VARCHAR(12) NOT NULL,
-    'scadenza_carta' DATE NOT NULL,
+    cf VARCHAR(16) NOT NULL PRIMARY KEY,
+    nome VARCHAR(20) NOT NULL,
+    fascia_reddito FLOAT NOT NULL,
+    sesso ENUM('M', 'F') NOT NULL,
+    data_nascita DATE NOT NULL,
+    indirizzo VARCHAR(30) NOT NULL,
+    saldo FLOAT NOT NULL,
+    numero_carta  VARCHAR(12) NOT NULL,
+    scadenza_carta DATE NOT NULL
 );
 
 
 --creazione tabella promozioni
 CREATE TABLE promozioni(
-    'codice' VARCHAR(16) NOT NULL PRIMARY KEY,
-    'nome_prodotto' VARCHAR(20) NOT NULL,
-    'costo_prodotto' FLOAT NOT NULL,
-    'data_inizio' DATE NOT NULL,
-    'data_fine' DATE NOT NULL,
+    codice VARCHAR(16) NOT NULL PRIMARY KEY,
+    nome_prodotto VARCHAR(20) NOT NULL,
+    costo_prodotto FLOAT NOT NULL,
+    data_inizio DATE NOT NULL,
+    data_fine DATE NOT NULL
 );
 
 --creazione tabella offerta (entità relazionale tra promozioni e clienti, ha come FK le PK di clienti e promozioni)
 CREATE TABLE offerte(
-    'cf' VARCHAR(16) NOT NULL, 
-    'codice' VARCHAR(16) NOT NULL, 
-    'risposta' ENUM('si', 'no') NOT NULL, 
+    cf VARCHAR(16) NOT NULL, 
+    codice VARCHAR(16) NOT NULL, 
+    risposta ENUM('si', 'no') NOT NULL, 
 
     FOREIGN KEY (cf) REFERENCES clienti(cf), 
     FOREIGN KEY (codice) REFERENCES promozioni(codice), 
@@ -38,30 +38,30 @@ CREATE TABLE offerte(
 
 --creazione tabella estratti conto (ha come FK la PK dei clienti)
 CREATE TABLE estratti_conto(
-    'numero' INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    'data' DATE NOT NULL,
-    'totale_spesa' FLOAT NOT NULL,
-    'cf' VARCHAR(16) NOT NULL,
-
+    numero INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    data DATE NOT NULL,
+    totale_spesa FLOAT NOT NULL,
+    cf VARCHAR(16) NOT NULL,
+    
     FOREIGN KEY (cf) REFERENCES clienti(cf)
 );
 
 --creazione tabella esercenti
 CREATE TABLE esercenti(
-    'id' INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    'nome' VARCHAR(20) NOT NULL,
-    'categoria' ENUM('supermercato', 'ristorante', 'autonoleggio', 'viaggi', 'intrattenimento', 'farmacia') NOT NULL, 
-    'citta' VARCHAR(30) NOT NULL, 
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(20) NOT NULL,
+    categoria ENUM('supermercato', 'ristorante', 'autonoleggio', 'viaggi', 'intrattenimento', 'farmacia') NOT NULL, 
+    citta VARCHAR(30) NOT NULL
 );
 
 --creazione tabella acquisti (ha come FK gli esercenti e gli estratti conto)
 CREATE TABLE acquisti(
-    'id' VARCHAR(15) NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-    'data' DATE NOT NULL, 
-    'importo' FLOAT NOT NULL, 
-    'esercente' INT NOT NULL, 
-    'estratto_conto' VARCHAR(15) NOT NULL, 
-    'cliente' VARCHAR(16) NOT NULL, 
+    id' INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+    data DATE NOT NULL, 
+    importo FLOAT NOT NULL, 
+    esercente INT NOT NULL, 
+    estratto_conto INT NOT NULL, 
+    cliente VARCHAR(16) NOT NULL, 
     
     FOREIGN KEY (esercente) REFERENCES esercenti(id),
     FOREIGN KEY (estratto_conto) REFERENCES estratti_conto(numero),
