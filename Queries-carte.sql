d2c0ef1ae3b5299aa13a2ceb988f1cd19c31448a
MrBarso <andbars2911@gmail.com>
2024-01-28 16:46:07 +0100

    paopeop

--- Queries-carte.sql
+++ Queries-carte.sql
@@ -8,16 +8,16 @@ JOIN esercenti ON acquisti.esercente
 WHERE esercenti.categoria = 'ESEMPIO'; -- cambiare esempio o parametro
 
 -- numero medio di acquisti di categoria "articolo da viaggio" dei clienti con reddito tra 50000 e 60000 in un certo anno per estratto conto e città dell'esercente
-/*SELECT 
+SELECT 
     AVG(IF(esercenti.categoria = 'viaggi', 1, NULL)) AS acquisti_viaggi_media
 FROM esercenti
-JOIN acquisti ON acquisti.esercente 
-AND estratto_conto ON acquisti.id 
+JOIN acquisti ON esercenti.id 
+AND estratto_conto ON esercenti.id 
 AND clienti ON acquisti.cliente
 WHERE 
-    YEAR(acquisti.data) = 2024 -- cambiare anno o parametro
+    YEAR(acquisti.data) = 'ESEMPIO' -- cambiare anno o parametro
     AND clienti.fascia_reddito >= 50000
-    AND clienti.fascia_reddito < 60000;*/
+    AND clienti.fascia_reddito < 60000;
 
 
 -- ammontare totale speso in viaggi dai clienti per mese della spesa e per provincia dell'esercente
